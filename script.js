f72fd624615a7b172f37d1f15a9fe5ebe16d9f1d
Ch Ihtasham <cihtasham32@gmail.com>
2024-07-31 13:16:18 +0500

    comit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = '5a5b9db67354136d5ab2c73981bb2ded';
 const searchBox = document.querySelector('.search input')
 const searchBtn = document.querySelector('.search button')
 const locationBtn = document.querySelector('.location')
+let weatherIcon = document.querySelector('.weather-icon')
 let city;
 let url;
 
@@ -18,7 +19,7 @@ locationBtn.addEventListener('click', () => {
     getLocation((lat, lon) => {
         fetchWeather(lat, lon)
     });
-    searchBox.value=''
+    searchBox.value = ''
 
 })
 
@@ -58,6 +59,18 @@ async function fetchWeather(lat, lon) {
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h';
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png"
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png"
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png"
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png"
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png"
+    }
+
 
 }
 
